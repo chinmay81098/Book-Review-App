6e32ac84970ce0073f30eb6d4d4f1d4d80ef4413
Chinmay Jain <chinmay81098@gmail.com>
2020-05-30 16:53:40 +0530

    added search feature

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -24,11 +24,20 @@ db = scoped_session(sessionmaker(bind=engine))
 @app.route("/")
 def index():
     books = db.execute("SELECT title FROM books limit 10").fetchall()
-    for book in books:
-        print(book.title)
     return render_template("index.html",books = books)
 
-@app.route("/home")
+@app.route("/home",methods=["GET","POST"])
 def home():
+    if request.method == "POST":
+        query = request.form.get("book")
+        results = db.execute("SELECT * FROM books WHERE title LIKE :query \
+            OR author LIKE :query OR book_id LIKE :query",{"query":query}).fetchall()
+        return render_template("home.html",results=results)
     #users = db.execute("SELECT id, username FROM users WHERE username = :username", {"username": name} )
     return render_template("home.html")
+
+@app.route("/bookPage/<string:id>",methods=["GET","POST"])
+def bookPage(id):
+    book = db.execute("SELECT * FROM books WHERE book_id = :id",{"id":id}).fetchone()
+    reviews = db.execute("SELECT reviews FROM book_reviews WHERE book_id = :id",{"id":id}).fetchall()
+    return render_template("book.html",reviews=reviews,book=book)
