d89cbb7c906eff4b3a107722076e4c4283545c57
Chinmay Jain <chinmay81098@gmail.com>
2020-06-13 21:12:36 +0530

    updated schema for book_review table

--- createtable.py
+++ createtable.py
@@ -10,6 +10,8 @@ db = scoped_session(sessionmaker(bind=engine))
 db.execute("CREATE TABLE users ( id SERIAL PRIMARY KEY,\
     username VARCHAR NOT NULL, password VARCHAR NOT NULL)")
 
-db.execute("CREATE TABLE book_reviews ( book_id VARCHAR REFERENCES books, reviews VARCHAR)")
+#db.execute("CREATE TABLE book_reviews ( book_id VARCHAR REFERENCES books, reviews VARCHAR)")
+
+db.execute("CREATE TABLE book_reviews (book_id VARCHAR REFERENCES books, username VARCHAR NOT NULL, rating INTEGER NOT NULL, text_reviews VARCHAR)")
 
 db.commit()
